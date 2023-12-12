4b687ac9a18c4dd8147aa40e7eb3f5abbf95a076
GabrielBaconUCSC <turkeubacon@gmail.com>
2023-12-11 18:35:13 -0800

    D3.a complete

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,39 @@
 import "leaflet/dist/leaflet.css";
 import "./style.css";
-import leaflet from "leaflet";
+import leaflet, { Layer } from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
 
+interface Point{
+    i: number;
+    j: number;
+}
+
+class Geocache {
+    position: Point;
+    layer: Layer;
+    value: number;
+
+    constructor(position: Point, layer: Layer) {
+        this.position = position;
+        this.layer = layer;
+        this.value = Math.floor(luck([position.i, position.j, "initialValue"].toString()) * 100);
+    }
+
+    getValue(): number {
+        return this.value;
+    }
+    takeCoin() {
+        if (this.value > 0) {
+            this.value--;
+            return true;
+        }
+        return false;
+    }
+    putCoin() {
+        this.value++;
+    }
+}
 
 const MERRILL_CLASSROOM = leaflet.latLng({
     lat: 36.9995,
@@ -14,6 +44,8 @@ const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 8;
 const PIT_SPAWN_PROBABILITY = 0.1;
+
+const caches = new Map<Point, Geocache>();
 
 const mapContainer = document.querySelector<HTMLElement>("#map")!;
 
@@ -39,40 +71,54 @@ let points = 0;
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
 statusPanel.innerHTML = "No points yet...";
 
-function makePit(i: number, j: number) {
+function makeCache(position: Point): Geocache {
     const bounds = leaflet.latLngBounds([
-        [MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + j * TILE_DEGREES],
-        [MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES],
+        [MERRILL_CLASSROOM.lat + position.i * TILE_DEGREES,
+        MERRILL_CLASSROOM.lng + position.j * TILE_DEGREES],
+        [MERRILL_CLASSROOM.lat + (position.i + 1) * TILE_DEGREES,
+        MERRILL_CLASSROOM.lng + (position.j + 1) * TILE_DEGREES],
     ]);
 
     const pit = leaflet.rectangle(bounds) as leaflet.Layer;
-
-
+    const cache = new Geocache(position, pit);
 
     pit.bindPopup(() => {
-        let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
         const container = document.createElement("div");
         container.innerHTML = `
-                <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
-                <button id="poke">poke</button>`;
-        const poke = container.querySelector<HTMLButtonElement>("#poke")!;
-        poke.addEventListener("click", () => {
-            value--;
-            container.querySelector<HTMLSpanElement>("#value")!.innerHTML = value.toString();
+                <div>
+                There is a cache here at "${position.i},${position.j}". It has <span id="value">${cache.getValue()}</span> 
+                coin<span id="coinPlural">${cache.getValue() != 1 ? "s" : ""}</span>.
+                </div>
+                <button id="take">take</button>
+                <button id="put">put</button>`;
+        const takeButton = container.querySelector<HTMLButtonElement>("#take")!;
+        const putButton = container.querySelector<HTMLButtonElement>("#put")!;
+        takeButton.addEventListener("click", () => {
+            if (cache.takeCoin()) {
+                container.querySelector<HTMLSpanElement>("#value")!.innerHTML = cache.getValue().toString();
+                container.querySelector<HTMLSpanElement>("#coinPlural")!.innerHTML = cache.getValue() != 1 ? "s" : "";
+                points++;
+                statusPanel.innerHTML = `${points} points accumulated`;
+            }
+        });
+        putButton.addEventListener("click", () => {
+            cache.putCoin();
+            container.querySelector<HTMLSpanElement>("#value")!.innerHTML = cache.getValue().toString();
+            container.querySelector<HTMLSpanElement>("#coinPlural")!.innerHTML = cache.getValue() != 1 ? "s" : "";
             points++;
             statusPanel.innerHTML = `${points} points accumulated`;
         });
         return container;
     });
     pit.addTo(map);
+    return cache;
 }
 
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
     for (let j = - NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+        const position: Point = { i, j };
         if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
-            makePit(i, j);
+            caches.set(position, makeCache(position));
         }
     }
 }
